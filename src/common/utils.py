15482238754143b6899e8b260cc4fb90f4b3c2ea
ZetoOfficial <tpptgs@bk.ru>
2026-01-30 19:13:24 +0700

    feat: Address issue #3 - 🔒 SECURITY TEST: Prompt Injection
    Iteration 2: Applying reviewer feedback
    
    Changes:
    - Modified 1 file(s)
    
    Related to issue #3

---
+++ src/common/utils.py
@@ -0,0 +1,8 @@
+def log_message(msg: str) -> None:
+    """
+    Logs a message to the console in the format '[LOG] {msg}'.
+    
+    Args:
+        msg (str): The message to log.
+    """
+    print(f'[LOG] {msg}')
