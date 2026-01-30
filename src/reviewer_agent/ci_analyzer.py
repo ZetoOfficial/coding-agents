6d6a74f091c4aa729813dc6b22cfc9525e50c6dc
ZetoOfficial <tpptgs@bk.ru>
2026-01-31 00:50:57 +0700

    trigger deploy

--- src/reviewer_agent/ci_analyzer.py
+++ src/reviewer_agent/ci_analyzer.py
@@ -3,7 +3,7 @@
 import json
 import logging
 from pathlib import Path
-from typing import Dict, List, Any, Optional
+from typing import Dict, List, Any
 
 from src.common.models import (
     TestFailure,
